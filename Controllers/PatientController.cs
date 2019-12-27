842262147f1a83d289853b8591efeeb711e8b2b3
danksduncan <danksduncan@gmail.com>
2019-12-27 16:23:41 -0500

    Initial Commit. New 'Create', 'Edit', 'Details', 'Delete' views

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,128 @@
+﻿using MVCPatientApp.Context;
+using MVCPatientApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace MVCPatientApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        private PatientContext db = new PatientContext();
+
+        // GET: Patient
+        public ActionResult Index()
+        {
+            return View(db.Patients.ToList());
+        }
+
+        // GET: Patient/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+                return HttpNotFound();
+            return View(patient);
+        }
+
+        // GET: Patient/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patient/Create
+        [HttpPost]
+        public ActionResult Create(Patient patient)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Patients.Add(patient);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(patient);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Patient/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+                return HttpNotFound();
+            return View(patient);
+        }
+
+        // POST: Patient/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Patient patient)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(patient).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(patient);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Patient/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+                return HttpNotFound();
+            return View(patient);
+        }
+
+        // POST: Patient/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int? id, Patient pat)
+        {
+            try
+            {
+                Patient patient = new Patient();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    patient = db.Patients.Find(id);
+                    if (patient == null)
+                        return HttpNotFound();
+                    db.Patients.Remove(patient);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(patient);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPatientApp.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Comments { get; set; }
+    }
+}
