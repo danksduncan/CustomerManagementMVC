a3055e7b6f41e2a5132f7b814164bd2653032bce
danksduncan <danksduncan@gmail.com>
2020-01-01 16:15:01 -0500

    Found bug relating to connection string of database. Bugs resolved, form now submits data to database and displays properly. Testing on 'Delete' option still needed. Once complete work on UI design + clean up unused folders and files in solution.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MVCPatientApp.Context;
+using MVCPatientApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +11,53 @@ namespace MVCPatientApp.Controllers
 {
     public class HomeController : Controller
     {
+        private PatientContext db = new PatientContext();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Patients.ToList());
         }
 
-        public ActionResult About()
+        public ActionResult ViewList()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return View(db.Patients.ToList());
         }
 
-        public ActionResult Contact()
+        public ActionResult Details(int? id)
         {
-            ViewBag.Message = "Your contact page.";
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+                return HttpNotFound();
+            return View(patient);
+        }
 
+        // GET: Patient/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
         }
+
+        // POST: Patient/Create
+        [HttpPost]
+        public ActionResult Create(Patient patient)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Patients.Add(patient);
+                    db.SaveChanges();
+                    return RedirectToAction("ViewList");
+                }
+                return View(patient);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
     }
 }
